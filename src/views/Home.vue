f466efa4abb2ff9e18cde7c5e927ee5e568da985
Kasper <kasperkh.kh@gmail.com>
2020-05-21 14:07:45 +0200

    Added embedded livestream

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,7 @@
 <template lang='pug'>
 .page
+  .yt-container
+    <iframe width="560" height="315" src="https://www.youtube.com/embed/live_stream?channel=UCQpQGT51tQbDQztiEH6hBcQ&autoplay=1" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
   img(src='@/assets/poster.jpg')
 </template>
 
@@ -17,6 +19,12 @@ img
   display: flex
   flex-direction: column
   align-items: center
+  .yt-container
+    width: 100%
+    max-width: 840px
+    iframe
+      width: 752px
+      height: 423px
   h1
     margin-top: 0px
 </style>
