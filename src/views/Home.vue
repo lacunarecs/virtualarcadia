7133f815bc543755a81c2f618869e5f5d7573281
Kasper <kasperkh.kh@gmail.com>
2020-05-23 01:27:56 +0200

    Replaced youtube livestream with twitch

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,10 @@
 <template lang='pug'>
 .page
   .yt-container
-    <iframe width="560" height="315" src="https://www.youtube.com/embed/live_stream?channel=UCQpQGT51tQbDQztiEH6hBcQ&autoplay=1" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+    //- <iframe width="560" height="315" src="https://www.youtube.com/embed/live_stream?channel=UCQpQGT51tQbDQztiEH6hBcQ&autoplay=1" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+    <iframe src="https://player.twitch.tv/?channel=lacunarecs&parent=www.example.com"></iframe>
+    p <a href='https://www.twitch.tv/lacunarecs' target='_blank'>Follow</a> on Twitch to be notified when streams start
+
   img(src='@/assets/poster.jpg')
 </template>
 
@@ -12,6 +15,10 @@ export default {
 </script>
 
 <style lang='sass' scoped>
+p
+  margin: 20px 0px
+  a
+    color: white
 img
   width: 100%
   max-width: 750px
@@ -25,6 +32,19 @@ img
     iframe
       width: 752px
       height: 423px
+      border: 0px
+      @media (max-width: 800px)
+        width: 640px
+        height: 360px
+      @media (max-width: 640px)
+        width: 512px
+        height: 288px
+      @media (max-width: 512px)
+        width: 384px
+        height: 216px
+      @media (max-width: 384px)
+        width: 256px
+        height: 144px
   h1
     margin-top: 0px
 </style>
